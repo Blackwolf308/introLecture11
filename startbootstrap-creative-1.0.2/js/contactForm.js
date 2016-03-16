6f85f9f3486dac3b43f620b6f460f42049490c5e
Blackwolf308 <blackwolf3012@live.com>
2016-03-15 23:34:13 -0400

    vcd

--- startbootstrap-creative-1.0.2/js/contactForm.js
+++ startbootstrap-creative-1.0.2/js/contactForm.js
@@ -18,3 +18,31 @@ function submitContactForm(){
         }
     })
 }
+//------------------------------------------
+//this is the main review from class #11
+
+$(document).ready(function(){
+    $("#thehidingform").addClass("hide");
+    $("#finalexam").on("click", function(){
+        $("#thehidingform").removeClass("hide");
+        $("#finalexam").addClass("hide");
+    })
+    $("#btnSubmit2").on("click", function(){
+        $.ajax({
+            url: "http://www.fakedoamin.com/formpost",
+            method: "POST",
+            data: $("#btnSubmit2").serialize(),
+            success: function(){
+                $("#btnSubmit2").text("Submitted");
+                alert("Congrats on the form!!!");
+            },
+            beforeSent: function(){
+                $("#btnSubmit2").text("Submitting");
+            },
+            error: function(){
+                $("#btnSubmit2").text("Retry");
+                alert("Form Submission Failed. Please try again");
+            }
+        });
+    });
+});
